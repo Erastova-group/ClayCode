c3509adcaca247895f5c2e2d7260fbe9a1dd09e5
s2123329 <h.pollak@sms.ed.ac.uk>
2023-02-16 13:55:00 +0000

    Add restructuring TODOs to __init__

---
+++ package/ClayCode/__init__.py
@@ -0,0 +1,5 @@
+__all__ = ['analysis', 'builder', 'siminp']
+# TODO: remove siminp?
+# TODO: take out setup/edit/molecule insertion from analysis
+# TODO: Now analysis has analysis but also checks
+# TODO: make config (for setup) become a base for all
