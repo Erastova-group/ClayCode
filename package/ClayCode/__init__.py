5083cfb0d04f5802d4138853b2c397e25dac8176
s2123329 <h.pollak@sms.ed.ac.uk>
2023-02-16 14:16:58 +0000

    Add new data

--- package/ClayCode/__init__.py
+++ package/ClayCode/__init__.py
@@ -3,3 +3,4 @@ __all__ = ['analysis', 'builder', 'siminp']
 # TODO: take out setup/edit/molecule insertion from analysis
 # TODO: Now analysis has analysis but also checks
 # TODO: make config (for setup) become a base for all
+
