c904f830b276b991c22bc9f26f8e2603ffafa211
s2123329 <h.pollak@sms.ed.ac.uk>
2023-04-20 19:36:46 +0200

    remove config

--- package/ClayCode/core/paths.py
+++ package/ClayCode/core/paths.py
@@ -37,42 +37,47 @@ MDP_DIR = Dir(resource_filename(__name__, "../data/mdp"))
 
 USER_DATA_DIR = Dir(resource_filename(__name__, "../data/user"), create=True)
 USER_FF_DIR = Dir(resource_filename(__name__, "../data/user/FF"), create=True)
-USER_CLAY_UNITS_DIR = Dir(resource_filename(__name__, "../data/user/clay_units"), create=True)
+USER_CLAY_UNITS_DIR = Dir(
+    resource_filename(__name__, "../data/user/clay_units"), create=True
+)
 USER_MDP_DIR = Dir(resource_filename(__name__, "../data/user/mdp"), create=True)
 
 WDIR = Dir.cwd()
 
 _MDP_DICT = {mdp.stem: File(mdp) for mdp in list(MDP_DIR.glob("*"))}
 
+
 def get_user_data():
-    for dir in globals()['USER_FF_DIR'], globals()['USER_CLAY_UNITS_DIR']:
-        entries = list(dir.glob(r'*'))
+    for dir in globals()["USER_FF_DIR"], globals()["USER_CLAY_UNITS_DIR"]:
+        entries = list(dir.glob(r"*"))
         if len(entries) != 0:
             for entry in entries:
-                print(f'Adding {entry.name!r} to {dir.name!r}')
+                print(f"Adding {entry.name!r} to {dir.name!r}")
                 print(entry.name)
                 if entry.name not in globals():
                     globals()[entry.name] = Dir[entry]
-                    globals()[f'_{dir.name}'].append(Dir(entry))
+                    globals()[f"_{dir.name}"].append(Dir(entry))
         else:
-            print(f'No user data to add to {dir.name!r}.')
-    data = list(USER_MDP_DIR.glob('.mdp'))
+            print(f"No user data to add to {dir.name!r}.")
+    data = list(USER_MDP_DIR.glob(".mdp"))
     for mdp in data:
-        globals()['MDP_DICT'][mdp.stem] = File(mdp)
+        globals()["MDP_DICT"][mdp.stem] = File(mdp)
+
 
 get_user_data()
 
-def add_new_data(path, kind=Literal[Union['FF', 'clay_units', 'mdp']]):
-    kind = f'_{kind}'
+
+def add_new_data(path, kind=Literal[Union["FF", "clay_units", "mdp"]]):
+    kind = f"_{kind}"
     path = Dir(path)
-    if kind == 'FF':
-        if path.suffix != '.ff':
+    if kind == "FF":
+        if path.suffix != ".ff":
             print(path.suffix)
             raise ValueError(f'Force field path must end in {".ff"!r}!')
         else:
             name = path.name
             dest = globals()
-    elif kind == 'clay_units':
+    elif kind == "clay_units":
         clay_itp = sorted(path.itp_filelist.stems)
         clay_gro = sorted(path.gro_filelist.stems)
         if not clay_itp == clay_gro:
@@ -80,26 +85,28 @@ def add_new_data(path, kind=Literal[Union['FF', 'clay_units', 'mdp']]):
         else:
             name = path.name
             dest = globals()
-    elif kind == 'mdp':
-        if not File(path).match_name(ext='.mdp'):
-            raise ValueError('Wrong file format!')
+    elif kind == "mdp":
+        if not File(path).match_name(ext=".mdp"):
+            raise ValueError("Wrong file format!")
         else:
             name = path.stem
             dest = MDP_DIR
     else:
         raise ValueError(f'kind must be in {["FF", "CLAY_UNITS", "mdp"]}')
-    dest_path = eval(f'USER_{kind.upper()}_DIR/path.name')
-    if dest_path.name in PathList(dest[f'{kind.upper()}_DIR']).names:
-        raise ValueError(f'{name} is already registered!')
+    dest_path = eval(f"USER_{kind.upper()}_DIR/path.name")
+    if dest_path.name in PathList(dest[f"{kind.upper()}_DIR"]).names:
+        raise ValueError(f"{name} is already registered!")
     else:
-        print(f'Adding {name!r} (from {str(path)!r}) to {str(dest_path.parent / dest_path.name)!r}')
+        print(
+            f"Adding {name!r} (from {str(path)!r}) to {str(dest_path.parent / dest_path.name)!r}"
+        )
         dest[path.name] = dest_path.resolve()
         shutil.copytree(path, dest_path, dirs_exist_ok=True)
         globals()[kind.upper()].append(globals()[path.name])
-        print(f'Updating internal data:')
+        print(f"Updating internal data:")
         get_user_data()
 
 
-add_ff = partial(add_new_data, kind='FF')
-add_uc = partial(add_new_data, kind='clay_units')
-add_mdp = partial(add_new_data, kind='mdp')
+add_ff = partial(add_new_data, kind="FF")
+add_uc = partial(add_new_data, kind="clay_units")
+add_mdp = partial(add_new_data, kind="mdp")
