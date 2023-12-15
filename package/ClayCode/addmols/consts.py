be1beb830e51e8e2e40314ee3c7f1d3e0ba2af55
s2123329 <h.pollak@sms.ed.ac.uk>
2023-12-15 10:45:36 +0000

    Add molecule addition constants

--- package/ClayCode/addmols/consts.py
+++ package/ClayCode/addmols/consts.py
@@ -4,8 +4,11 @@ from pathlib import Path
 
 import yaml
 
-__all__ = ["ADDMOLS_DEFAULTS"]
+__all__ = ["ADDMOLS_DEFAULTS", "ADDTYPES"]
 
 defaults_file = Path(__file__).parent / "config/defaults.yaml"
 with open(defaults_file, "r") as file:
     ADDMOLS_DEFAULTS = yaml.safe_load(file)
+
+with open(Path(__file__).parent / "config/addtypes.yaml", "r") as file:
+    ADDTYPES = yaml.safe_load(file)
